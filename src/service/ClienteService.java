9b66cb8e35040ffc7ca50858eab3b4ed4834c72b
Rafael <RAFADOMANGUE>
2026-06-22 00:10:56 -0300

    Feito a lógica para cadastrar cliente, cliente service vai ser chamado pelo java Swing para fazer a validações dos dados: nome, telefone, se for válido clienteService vai chamar o repository e guardar o cliente em uma lista. Na lista feito a lógica de contador, cada cliente novo vai ser setado um id diretamente no repository, para caso seja excluido um cliente, não tenha interferencia em alguma busca. Feito apenas o cadastro, exclusão e busca vai ser feito posteriormente.

--- src/service/ClienteService.java
+++ src/service/ClienteService.java
@@ -1,4 +1,28 @@
 package service;
 
+import model.Cliente;
+import repository.RepositoryMemoria;
+
 public class ClienteService {
+    private RepositoryMemoria repositoryMemoria = new RepositoryMemoria();
+    public void cadastrarCliente(String nome, String telefone){
+        try{
+            if(nome == null || nome.isBlank()){
+                throw new IllegalArgumentException("Nome não pode ser vazio");
+            }
+            if(telefone == null || telefone.isBlank()){
+                throw new IllegalArgumentException("Telefone não pode ser nulo");
+            }
+            if(telefone.length() != 11){
+                throw new IllegalArgumentException("Numero inválido");
+            }
+            Cliente cliente = new Cliente(nome, telefone);
+            repositoryMemoria.salvaCliente(cliente);
+
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+
+    }
 }
