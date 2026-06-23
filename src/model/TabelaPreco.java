ed762d8f213a6d3d365564733209ec0279d30b82
Rafael <RAFADOMANGUE>
2026-06-22 23:48:58 -0300

    freature: Criando algumas classes.

--- src/model/TabelaPreco.java
+++ src/model/TabelaPreco.java
@@ -1,4 +1,46 @@
 package model;
 
+import model.TipoPizza;
+
 public class TabelaPreco {
+    private double precoSimples;
+    private double precoEspecial;
+    private double precoPremium;
+
+    public TabelaPreco(){
+    }
+    public double getPrecoSimples(){
+        return this.precoSimples;
+    }
+
+    public double getPrecoEspecial() {
+        return precoEspecial;
+    }
+
+    public double getPrecoPremium() {
+        return precoPremium;
+    }
+    public void setPrecoSimples(double precoSimples) {
+        this.precoSimples = precoSimples;
+    }
+
+    public void setPrecoEspecial(double precoEspecial) {
+        this.precoEspecial = precoEspecial;
+    }
+
+    public void setPrecoPremium(double precoPremium) {
+        this.precoPremium = precoPremium;
+    }
+    public double getPrecoPorTipo(TipoPizza tipo) {
+        switch (tipo) {
+            case SIMPLES:
+                return this.precoSimples;
+            case ESPECIAL:
+                return this.precoEspecial;
+            case PREMIUM:
+                return this.precoPremium;
+            default:
+                throw new IllegalArgumentException("Tipo inválido");
+        }
+    }
 }
--- src/model/Triangulo.java
+++ src/model/Triangulo.java
@@ -1,4 +1,26 @@
 package model;
 
-public class Triangulo {
+public class Triangulo implements Forma{
+    private double lado;
+    public void Triangulo(double lado){
+        this.lado = lado;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public double calculaArea(){
+        return (Math.pow(this.lado,2) * Math.sqrt(3))/4;
+    }
+    @Override
+    public String descricao(){
+        return "lado de: " + lado;
+    }
+
 }
