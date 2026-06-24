225d727548b821c721d9b2a4d610cd91764a5e11
Calebe Carozzi <calebercarozzi@outlook.com>
2026-06-24 00:06:29 -0300

    feat: implementacao da classe PrecoService e do preco em Repository

--- src/repository/RepositoryMemoria.java
+++ src/repository/RepositoryMemoria.java
@@ -5,6 +5,7 @@ import exception.ClienteNaoEncontradoException;
 import exception.SaborInvalidoException;
 import model.Cliente;
 import model.Sabor;
+import model.TabelaPreco;
 import model.TipoPizza;
 
 import java.util.ArrayList;
@@ -12,60 +13,78 @@ import java.util.List;
 
 public class RepositoryMemoria {
 
-    //==== CLIENTE ==============================================================================
+    //=== ATRIBUTOS ============================================================================
 
+    //---CLIENTES-----------------------------------------
+    private List<Sabor> sabores = new ArrayList<>();
+    int contadorSabor = 1;
 
-    // Salvar cliente ---------------------------------------------------------------------------
+    //---SABORES------------------------------------------
     private List<Cliente> clientes = new ArrayList<>();
     int contadorCliente = 1;
 
-    public void salvaCliente(Cliente cliente){
+    //---PRECOS--------------------------------------------
+    private TabelaPreco tabelaPreco = new TabelaPreco();
+    //==========================================================================================
+
+
+    //=== GETs =================================================================================
+
+    // --- não sei se vai precisar colocar coisa aqui
+
+
+    //==========================================================================================
+
+
+
+
+
+
+    //==== CLIENTE ==============================================================================
+
+    // Salvar cliente ---------------------------------------------------------------------------
+
+    public void salvaCliente(Cliente cliente) {
         cliente.setId(contadorCliente);
         this.clientes.add(cliente);
         contadorCliente++;
     }
-    //--------------------------------------------------------------------------------------------
 
 
     // Excluir cliente ---------------------------------------------------------------------------
-    public void excluiCliente(int idCliente){
+    public void excluiCliente(int idCliente) {
         boolean removeu = clientes.removeIf(c -> c.getId() == idCliente);
 
         if (!removeu) {
             throw new ClienteNaoEncontradoException("Cliente não encontrado");
         }
     }
-    //--------------------------------------------------------------------------------------------
 
 
     // Editar cliente ---------------------------------------------------------------------------
-    public void editarCliente(int idCliente, String nome, String sobrenome, String telefone){
+    public void editarCliente(int idCliente, String nome, String sobrenome, String telefone) {
         for (Cliente cliente : clientes) {
             if (cliente.getId().equals(idCliente)) {
-                if(!nome.isBlank()){
+                if (!nome.isBlank()) {
                     cliente.setNome(nome);
                 }
-                if(!sobrenome.isBlank()){
+                if (!sobrenome.isBlank()) {
                     cliente.setSobrenome(sobrenome);
                 }
-                if(!telefone.isBlank()){
-                cliente.setTelefone(telefone);
+                if (!telefone.isBlank()) {
+                    cliente.setTelefone(telefone);
                 }
                 return;
             }
         }
         throw new ClienteNaoEncontradoException("Cliente não encontrado");
     }
-    //--------------------------------------------------------------------------------------------
 
 
     // retorna clientes  -------------------------------------------------------------------------
     public List<Cliente> retornaListaCliente() {
         return clientes;
     }
-    //--------------------------------------------------------------------------------------------
-
-
 
     // ==============================================================================================
 
@@ -73,38 +92,31 @@ public class RepositoryMemoria {
 
 
 
-
     //==== SABORES DA PIZZA ==========================================================================
 
-
     // Salvar sabores ---------------------------------------------------------------------------
-    private List<Sabor> sabores = new ArrayList<>();
-    int contadorSabor = 1;
-
-    public void salvaSabor(Sabor sabor){
+    public void salvaSabor(Sabor sabor) {
         sabor.setIdSabor(contadorSabor);
         this.sabores.add(sabor);
         contadorSabor++;
     }
-    //--------------------------------------------------------------------------------------------
 
 
     // Excluir sabor ---------------------------------------------------------------------------
-    public void excluiSabor(int idSabor){
+    public void excluiSabor(int idSabor) {
         boolean removeu = sabores.removeIf(s -> s.getIdSabor() == idSabor);
 
         if (!removeu) {
             throw new SaborInvalidoException("Sabor não encontrado");
         }
     }
-    //--------------------------------------------------------------------------------------------
 
 
     // Editar sabor ---------------------------------------------------------------------------
-    public void editarSabor(int idSabor, String nome, TipoPizza tipo){
-        for (Sabor sabor: sabores) {
+    public void editarSabor(int idSabor, String nome, TipoPizza tipo) {
+        for (Sabor sabor : sabores) {
             if (sabor.getIdSabor().equals(idSabor)) {
-                if(!nome.isBlank()){
+                if (!nome.isBlank()) {
                     sabor.setNome(nome);
                 }
                 sabor.setTipoPizza(tipo);
@@ -113,18 +125,54 @@ public class RepositoryMemoria {
         }
         throw new SaborInvalidoException("Sabor não encontrado");
     }
-    //--------------------------------------------------------------------------------------------
 
 
     // retorna sabor  -------------------------------------------------------------------------
     public List<Sabor> retornaListaSabor() {
         return sabores;
     }
-    //--------------------------------------------------------------------------------------------
-
-
-
-
 
     // ==============================================================================================
+
+
+
+
+    // === TABELA PRECOS =========================================================================================
+
+    // retorna tebela preco  -------------------------------------------------------------------------
+    public TabelaPreco retornaTabelaPrecos() {
+        return this.tabelaPreco;
+    }
+
+    // retorna preco por tipo  -------------------------------------------------------------------------
+    public double retornaPrecoPorTipo(TipoPizza tipo) {
+        return this.tabelaPreco.getPrecoPorTipo(tipo);
+    }
+
+    // editar preco  -------------------------------------------------------------------------
+    public void editarPreco(TipoPizza tipo, double valor) {
+        this.tabelaPreco.alterarPreco(tipo, valor);
+    }
+
+    // ==============================================================================================
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/service/PrecoService.java
+++ src/service/PrecoService.java
@@ -1,4 +1,68 @@
 package service;
 
+import exception.CampoInvalidoException;
+import exception.PrecoInvalidoException;
+import model.TabelaPreco;
+import model.TipoPizza;
+import repository.RepositoryMemoria;
+
 public class PrecoService {
+
+    RepositoryMemoria repositoryMemoria;
+
+    public PrecoService(RepositoryMemoria repositoryMemoria) {
+        this.repositoryMemoria = repositoryMemoria;
+    }
+
+    public TabelaPreco retornaTabelaPreco(){
+        return repositoryMemoria.retornaTabelaPrecos();
+    }
+
+    public double getPrecoSimples(){
+        return repositoryMemoria.retornaTabelaPrecos().getPrecoSimples();
+    }
+
+    public double getPrecoEspecial(){
+        return repositoryMemoria.retornaTabelaPrecos().getPrecoEspecial();
+    }
+
+    public double getPrecoPremium(){
+        return repositoryMemoria.retornaTabelaPrecos().getPrecoPremium();
+    }
+
+    public double getPrecoPorTipo(TipoPizza tipo){
+        validarTipo(tipo);
+        return repositoryMemoria.retornaPrecoPorTipo(tipo);
+    }
+
+    public void alterarPreco(TipoPizza tipo, double valor) {
+        validarTipo(tipo);
+        validarPreco(valor);
+
+        repositoryMemoria.editarPreco(tipo, valor);
+    }
+
+    public void alterarTodosPrecos(double precoSimples, double precoEspecial, double precoPremium){
+
+        validarPreco(precoSimples);
+        validarPreco(precoEspecial);
+        validarPreco(precoPremium);
+
+        repositoryMemoria.editarPreco(TipoPizza.SIMPLES, precoSimples);
+        repositoryMemoria.editarPreco(TipoPizza.ESPECIAL, precoEspecial);
+        repositoryMemoria.editarPreco(TipoPizza.PREMIUM, precoPremium);
+
+    }
+
+    public void validarTipo(TipoPizza tipo) {
+        if (tipo == null) {
+            throw new CampoInvalidoException("Tipo de pizza é obrigatório.");
+        }
+    }
+
+    public void validarPreco(double valor){
+        if (valor <= 0) {
+            throw new PrecoInvalidoException("O preço deve ser maior que zero.");
+        }
+    }
 }
