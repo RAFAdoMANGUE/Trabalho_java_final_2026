71c0e61f21020bbf6a732c41a9b46e5bca52e4ea
Calebe Carozzi <calebercarozzi@outlook.com>
2026-06-23 22:47:27 -0300

    feat: implementacao basica classe itemPedido

--- src/model/ItemPedido.java
+++ src/model/ItemPedido.java
@@ -2,4 +2,27 @@ package model;
 
 public class ItemPedido {
 
+    private Pizza pizza;
+    private int quantidade;
+
+    public ItemPedido(Pizza pizza, int quantidade) {
+        this.pizza = pizza;
+        this.quantidade = quantidade;
+    }
+
+    public Pizza getPizza() {
+        return pizza;
+    }
+
+    public void setPizza(Pizza pizza) {
+        this.pizza = pizza;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
 }
